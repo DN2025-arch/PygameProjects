7b1a15cd4df5c4bd8a4d1ea0306149a17a523e8a
DN2025-arch <daneloo2175@gmail.com>
2026-01-20 17:03:46 +0000

    Delete Zoom19,3 - FileCounter.py

--- Zoom19,3 - FileCounter.py
+++
@@ -1,27 +0,0 @@
-
-
-file_path = r"C:\Users\flori\OneDrive\Desktop\Python\CounterFile.txt"
-
-##file = open(file_path, "w")
-
-content = """
-This is line 1.
-This is line 2.
-Python file handling is easy.
-You can read this file.
-You can write new data
-You can append more text
-t.End of file
-"""
-##file.write(content)
-##
-##file.close()
-
-
-file = open(file_path, "r")
-content = file.readlines()
-print(content)
-wordcount = len(content)
-print(wordcount)
-
-file.close()
